a38a07b4fdf9338bd83a0f465c4ea3d3e62d7e5f
clowmin <acnmin@naver.com>
2015-03-06 20:28:02 +0900

    1

---
+++ Day0306/src/UnaryOperator.java
@@ -0,0 +1,20 @@
+
+public class UnaryOperator {
+
+	public static void main(String[] args) {
+
+		int opr =0;
+		opr+=3;  //3
+		System.out.println(opr++); //3, 4
+		System.out.println(opr); //4
+		System.out.println(++opr); //5
+		System.out.println(opr); //5
+		
+		System.out.println(opr--); //5 //4
+		System.out.println(opr); //4
+		System.out.println(--opr); //3
+		System.out.println(opr); //3
+	
+	}
+
+}

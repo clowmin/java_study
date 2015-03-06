a3eed439c7b9775ec2f5a9b1f2061c158e009998
kakaotalk <acnmin@naver.com>
2015-03-06 09:34:43 +0900

    a

--- TestHello/src/SayHello.java
+++ TestHello/src/SayHello.java
@@ -6,7 +6,7 @@ public class SayHello {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello World  haha");
+System.out.println("Hello World  haha 123");
 	}
 
 }
